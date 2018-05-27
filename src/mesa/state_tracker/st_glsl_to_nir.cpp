14e06657b5707d27350fe5636c74e9ba6a7e5a87
Vasily Khoruzhick <anarsoul@gmail.com>
2018-05-27 12:08:06 -0700

    DO-NOT-MERGE: disable splitting VS outputs/FS inputs
    Otherwise FS fails to compile due to increased register pressure
    
    Signed-off-by: Vasily Khoruzhick <anarsoul@gmail.com>

--- src/mesa/state_tracker/st_glsl_to_nir.cpp
+++ src/mesa/state_tracker/st_glsl_to_nir.cpp
@@ -650,7 +650,7 @@ st_link_nir(struct gl_context *ctx,
          mask = (nir_variable_mode)(mask | nir_var_shader_out);
 
       nir_shader *nir = shader->Program->nir;
-      NIR_PASS_V(nir, nir_lower_io_to_scalar_early, mask);
+      //NIR_PASS_V(nir, nir_lower_io_to_scalar_early, mask);
       st_nir_opts(nir);
    }
 
